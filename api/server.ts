3c0957aecec800fb6fc5ea70d4a98a988267fd11
Błażej Domagała <petitoff@outlook.com>
2023-10-22 13:44:27 +0200

    Revert "feat in progress: auto saving links from twtich"
    This reverts commit 5dadf808c51b4db8012ac90de2c1603645b7139c.

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,7 @@
 import fastify, {
-    FastifyInstance,
-    FastifyReply,
-    FastifyRequest,
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
 } from "fastify";
 import cors from "@fastify/cors";
 import fs from "fs";
@@ -24,7 +24,7 @@ const savedMessages: IMessage[] = [];
 const userColors: { [key: string]: string } = {};
 
 server.register(cors, {
-    origin: true,
+  origin: true,
 });
 
 server.post(
@@ -61,82 +61,36 @@ server.post(
 );
 
 server.get("/api/messages", async (_: FastifyRequest, reply: FastifyReply) => {
-    reply.send(messages);
+  reply.send(messages);
 });
 
 server.post(
-    "/api/save-message",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-        const message = request.body as IMessage;
-        savedMessages.push(message);
+  "/api/save-message",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    const message = request.body as IMessage;
+    savedMessages.push(message);
 
-        fs.writeFile("messages.json", JSON.stringify(savedMessages), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
+    fs.writeFile("messages.json", JSON.stringify(savedMessages), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
 
-        reply.send(savedMessages);
-    }
-);
-
-server.post(
-    "/api/save-messages/links",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-        const message = request.body as IMessage;
-
-        const EXCLUDED_AUTHORS = [
-            "StreamElements",
-            "Streamlabs",
-            "Nightbot",
-            "Streamlabs",
-            "Programmer_Network",
-        ];
-
-        if (EXCLUDED_AUTHORS.includes(message.author)) {
-            return;
-        }
-        try {
-            fs.readFile("links.json", "utf8", (err, data) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    // Parse the file content to an array
-                    const messages = JSON.parse(data || "[]");
-
-                    messages.push(message);
-
-                    fs.writeFile(
-                        "links.json",
-                        JSON.stringify(messages),
-                        "utf8",
-                        (err) => {
-                            if (err) {
-                                console.log(err);
-                            }
-                        }
-                    );
-                }
-            });
-        } catch (err) {
-            console.log(err);
-        }
-
-        reply.send(savedMessages);
-    }
+    reply.send(savedMessages);
+  }
 );
 
 const start = async () => {
-    try {
-        await server.listen({
-            port: 3000,
-            host: "::",
-        });
-        server.log.info(`Server listening on http://localhost:3000/`);
-    } catch (err) {
-        server.log.error(err);
-        process.exit(1);
-    }
+  try {
+    await server.listen({
+      port: 3000,
+      host: "::",
+    });
+    server.log.info(`Server listening on http://localhost:3000/`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
